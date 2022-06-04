3839717d39bb78972066c302c619105774617bce
김승민 <103026404+turfguy@users.noreply.github.com>
2022-06-04 20:47:25 +0900

    [Silver V] Title: 수 정렬하기 3, Time: 9396 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,11 @@
+import sys
+
+N = int(sys.stdin.readline())
+list = [0] * 10001
+
+for i in range(N):
+    list[int(sys.stdin.readline())]+=1
+for i in range(10001):
+    if list[i] != 0:
+        for j in range(list[i]):
+            print(i)
